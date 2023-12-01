e26a6f15857120ab8d9d5b814c966cc21f939c1b
bagasw980 <bagasw980@gmail.com>
2023-12-01 13:48:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <button class="btn-primary">Simpan</button>
+    <p></p>
+    <div class="box"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/index.scss";
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: $colorBackgroundBgFilledSuccess;
+}
+</style>
